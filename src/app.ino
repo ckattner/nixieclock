993b6959deaabc435b93c6e0c9afed86f13e80f4
Craig Kattner <2313730+ckattner@users.noreply.github.com>
2019-01-12 21:45:31 -0600

    Simple test sketch to test LED backlights on exixe boards

--- src/app.ino
+++ src/app.ino
@@ -1,29 +1,41 @@
-#include <DS3231.h>
+#include "exixe.h"
 
-DS3231 clock;
+int cs1 = 22;
+int cs2 = 23;
+int cs3 = 24;
+int cs4 = 25;
+int cs5 = 26;
+int cs6 = 27;
+
+exixe tube1 = exixe(cs1);
+exixe tube2 = exixe(cs2);
+exixe tube3 = exixe(cs3);
+exixe tube4 = exixe(cs4);
+exixe tube5 = exixe(cs5);
+exixe tube6 = exixe(cs6);
 
 void setup()
 {
-  Serial.begin(9600);
+  tube1.spi_init();
 
-  Serial.println("Initialize RTC");
-
-  clock.begin();
-
-  // __DATE__ and __TIME__ will be expanded at compile time.
-  clock.setDateTime(__DATE__, __TIME__);    
+  tube1.clear();
+  tube2.clear();
+  tube3.clear();
+  tube4.clear();
+  tube5.clear();
+  tube6.clear();
 }
+
+int color = 127;
 
 void loop()
 {
-  RTCDateTime dt = clock.getDateTime();
-
-  Serial.print(dt.month);  Serial.print("/");
-  Serial.print(dt.day);    Serial.print("/");
-  Serial.print(dt.year);   Serial.print(" ");
-  Serial.print(dt.hour);   Serial.print(":");
-  Serial.print(dt.minute); Serial.print(":");
-  Serial.print(dt.second); Serial.println("");
+  tube1.set_led(0, color, 0);
+  tube2.set_led(0, 0, color);
+  tube3.set_led(color, 0, 0);
+  tube4.set_led(0, color, 0);
+  tube5.set_led(0, 0, color);
+  tube6.set_led(color, 0, 0);
 
   delay(1000);
 }
